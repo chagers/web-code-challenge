230a589d70b8000269be90e6fa185d6382be1989
chagers <cehager@gmail.com>
2016-05-05 19:27:46 -0700

    refactor html and css; start adding animations - in progress

--- scripts/index.js
+++ scripts/index.js
@@ -2,4 +2,32 @@ $(document).ready(function() {
   $(".menu-icon").click(function() {
     $(".drop-down").toggle("slow");
   });
-})
+
+  // $(".btn").click(function() {
+  //   $(this).animate(
+  //     {backgroundColor: "black"},
+  //     500,
+  //     "swing",
+  //     function() {
+  //       $(this).css("background-color", "#1160AD");
+  //     }
+  //   );
+  // });
+$("button").click(function(){
+  $(this).css("backgroundColor", "#7F2B0D")
+  }
+);
+
+$(".menu-icon").click(function() {
+  $(this).animate(
+    {"rotate": "90"},
+    1000,
+    function() {
+      $(this).animate(
+        {"rotate": "90"}
+      )
+    }
+  );
+});
+
+});
