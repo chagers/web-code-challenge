1867a365605f710d0586936c8435feb29e37c20b
chagers <cehager@gmail.com>
2016-05-06 13:12:52 -0700

    final version

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,39 @@
 $(document).ready(function() {
+
+// dropdown menu animations on tablet and mobile
   $(".menu-icon").click(function() {
+    $(this).toggleClass("rotate");
     $(".drop-down").toggle("slow");
   });
 
-  // $(".btn").click(function() {
-  //   $(this).animate(
-  //     {backgroundColor: "black"},
-  //     500,
-  //     "swing",
-  //     function() {
-  //       $(this).css("background-color", "#1160AD");
-  //     }
-  //   );
-  // });
-$("button").click(function(){
-  $(this).css("backgroundColor", "#7F2B0D")
-  }
-);
+// button animations
+  $(".btn-red").click(function(){
+    $(this).css("backgroundColor", "#7F2B0D");
+  });
 
-$(".menu-icon").click(function() {
-  $(this).animate(
-    {"rotate": "90"},
-    1000,
-    function() {
-      $(this).animate(
-        {"rotate": "90"}
-      )
+  $(".btn-blue").click(function() {
+    $(this).css("backgroundColor", "#002B5C");
+  });
+
+// form actions and validation
+  $("#get-advice, #get-beer").click(function() {
+    var favoriteBeer = $("#fav_beer").val();
+    var beerCount = $("#beer_count").val();
+    if (favoriteBeer === "" || beerCount === undefined) {
+      alert("Enter your selections before we can give you advice or beer!");
+      return false;
     }
-  );
-});
+    var thisMany = function() {
+      if (beerCount === 1) {
+        return "1-2";
+      } else if (beerCount === 2) {
+        return "3-4";
+      } else {
+        return "5+";
+      }
+    };
+    console.log("The user's favorite beer is: " + favoriteBeer);
+    console.log("The user drinks " + thisMany() + " beers per week.");
+  });
 
 });
